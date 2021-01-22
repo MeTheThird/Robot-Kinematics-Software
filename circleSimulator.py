f4438cf5c7f8372764ecb6fe008bb79a06b07194
Rohan K. Thakur <rohan.k.thakur@gmail.com>
2021-01-21 17:17:42 -0800

    Update circleSimulator.py

--- circleSimulator.py
+++ circleSimulator.py
@@ -6,8 +6,8 @@ degrees = np.linspace(0, 2*np.pi, numDeg)
 r = 1
 x, y = [], []
 fig, ax = plt.subplots()
-# plt.axes().set_aspect('equal')
-plt.axis([-1.0, 1.0, -1.0, 1.0])
+ax.axis('equal')
+ax.set(xlim=(-2,2), ylim=(-2,2))
 
 for t in degrees:
     x.append(r*np.cos(t))
